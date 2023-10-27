3a85cd94274ebf6006f9ef07accf635f34a9236f
wangtz <wangtz19@outlook.com>
2023-10-27 11:57:16 +0000

    fix evaluate for multiclass classification

--- src/classification/src/utils.py
+++ src/classification/src/utils.py
@@ -40,8 +40,19 @@ def get_id2label(label2id):
 
 ID2LABEL = get_id2label(LABEL2ID)
 
-clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+# clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+average = "weighted"
+accuracy_metric = evaluate.load("accuracy", average=average)
+precision_metric = evaluate.load("precision", average=average)
+recall_metric = evaluate.load("recall", average=average)
+f1_metric = evaluate.load("f1", average=average)
+
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
-    return clf_metrics.compute(predictions=predictions, references=labels)
+    results = {}
+    results.update(accuracy_metric.compute(predictions=predictions, references=labels))
+    results.update(precision_metric.compute(predictions=predictions, references=labels, average=average))
+    results.update(recall_metric.compute(predictions=predictions, references=labels, average=average))
+    results.update(f1_metric.compute(predictions=predictions, references=labels, average=average))
+    return results
